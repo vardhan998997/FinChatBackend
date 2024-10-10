30476fc2c19a9e7c9a7d940e2afa7bbd1e7bb002
Jaswanth Sudha <119607624+JaswanthSudha@users.noreply.github.com>
2024-10-10 16:14:02 +0530

    uer

--- models/user.model.js
+++ models/user.model.js
@@ -18,4 +18,4 @@ const userSchema = new mongoose.Schema(
 	},
 	{ timestamps: true },
 );
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model('UserBa', userSchema);
